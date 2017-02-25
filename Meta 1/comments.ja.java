fcc434c8d9fb704f6033a1cb12c6849c0459d5da
Fernando Rocha <fmrocha@student.dei.uc.pt>
2017-02-25 18:43:09 +0000

    55 points so far

---
+++ Meta 1/comments.ja.java
@@ -0,0 +1,28 @@
+/*/*/***/*/*/*/*/
+
+sadnoas*/
+
+/*/***/*/*/*/*/
+
+//ajdoiasdoasmiodasidas*/asiodasjdas
+
+/*
+
+jdioasjdas
+
+//jaidosajidas /*adoisajdisajdias
+
+/*asodijasiodjasidsasioajdisa
+
+//osjdioasjdias
+*/
+
+/* this comment /* // /** ends here: */
+
+//* Fast flip comment starts here
+    ... code ...
+/* and ends here */
+
+/* Fast flip comment starts here
+    ... code ...
+/* and ends here */oi.
